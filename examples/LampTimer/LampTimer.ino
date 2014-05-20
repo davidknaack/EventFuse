4498d57c6c208f3f74501bf18829342ef0b98b3a
David Knaack <davidknaack@gmail.com>
2014-05-19 22:52:50 -0500

    minor updates

--- examples/LampTimer/LampTimer.ino
+++ examples/LampTimer/LampTimer.ino
@@ -12,24 +12,24 @@
 #include <EventFuse.h>
 #include <MsTimer2.h>
 
-#define OutputCount 4
-#define OffTime 0
-#define OnTime 1
-#define OutputPin 2
+#define OUTPUT_COUNT 4
+#define OFF_TIME 0
+#define ON_TIME 1
+#define OUTPUT_PIN 2
 
 // The outputs array defines how long each output will
 // be turned off, on, and what pin to use for that output.
 // The off and on values are in units of 'ticks'. The length
 // of a tick is controlled by the setup of MsTimer2. 
                              // off   on  pin
-byte outputs[OutputCount][3] ={{  5,  10,  13},   // Output A
-                               { 15,  20,  12},   // Output B
-                               {  2,  12,  11},   // Output C
-                               { 10,   2,  10},}; // Output D
+byte outputs[OUTPUT_COUNT][3] ={{  5,  10,  2},   // Output A
+                               { 15,  20,  3},   // Output B
+                               {  2,  12,  4},   // Output C
+                               { 10,   2,  5},}; // Output D
                     
 void OutputHandler(FuseID fuseID, int& outputID){
   // look up the pin associated with this output
-  byte pin = outputs[outputID][OutputPin];
+  byte pin = outputs[outputID][OUTPUT_PIN];
 
   // get and invert the current pin state and write
   // it back to the port to invert the current pin state.
@@ -42,17 +42,20 @@ void OutputHandler(FuseID fuseID, int& outputID){
 }
 
 void setup() {
+  Serial.begin(115200);
+  
   // Set up and init all outputs to off
-  for(byte i = 0; i<OutputCount; i++){
-    pinMode( outputs[i][OutputPin], OUTPUT);
-    digitalWrite( outputs[i][OutputPin], LOW );
+  for(byte i = 0; i<OUTPUT_COUNT; i++){
+    Serial.println(i);
+    pinMode( outputs[i][OUTPUT_PIN], OUTPUT);
+    digitalWrite( outputs[i][OUTPUT_PIN], LOW );
 
     // Set up an event fuse for this output.
-    EventFuse::newFuse( i, outputs[i][OffTime], INF_REPEAT, OutputHandler );
+    EventFuse::newFuse( i, outputs[i][OFF_TIME], INF_REPEAT, OutputHandler );
   }
   
   // Set MsTimer2 for one second per tick.
-  MsTimer2::set(1000, EventFuse::burn );
+  MsTimer2::set(100, EventFuse::burn );
   MsTimer2::start();
 }
 
